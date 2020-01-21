049967658439353fd781d1954b8b06e166b3fdcd
Alex DeBolt <58086131+alexdebolt@users.noreply.github.com>
2020-01-21 10:08:00 -0500

    Finish adding tracks to playlist, no error

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,34 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+      ], playlistName: 'Peanut Butter and Jamz'
+      , playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 4},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 5},
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 6}
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this)
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    // checking to see if track is already in playlist
+    if(tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } 
+    // if track is not in playlist, this adds track to playlist and sets new state
+    tracks.push(track)
+    this.setState({playlistTracks: tracks})
+  }
+  
   render() {
     return (
       <div>
@@ -13,8 +41,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,21 +3,33 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.addTrack = this.addTrack.bind(this);
+    }
+
+    // Setting the add track '+' if it has not yet been added and the '-' if it has
     renderAction() {
         if (this.props.isRemoval) {
             return <button className="Track-action">-</button>
         } else {
-            return <button className="Track-action">+</button>
+            //calls addTrack when + is clicked to add track from results to playlist
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
         }
+    }
+    // adds track to playlist from results
+    addTrack() {
+        this.props.onAdd(this.props.track);
     }
 
     render() {
         return (
             <div className="Track">
               <div className="Track-information">
-                <h3>track name</h3>
-                <p>track artist | track album </p>
+                <h3>{this.props.track.name}</h3>
+                <p>{this.props.track.artist} | {this.props.track.album} </p>
               </div>
               {this.renderAction()}
             </div>
