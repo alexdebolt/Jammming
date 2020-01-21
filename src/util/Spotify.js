ee8f627ce131702e2d9993d365cdb61a0573590b
Alex DeBolt <58086131+alexdebolt@users.noreply.github.com>
2020-01-21 12:12:01 -0500

    Add user access token details

---
+++ src/util/Spotify.js
@@ -0,0 +1,35 @@
+const cliendId = '7290845fe6f8489cb755e5a303b92afe';
+const redirectUri = 'http://localhost:3000/';
+
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            // Clearing parameters from URL so Jammming doesn't try to grab the token after it expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+
+    
+};
+
+
+
+export default Spotify;
